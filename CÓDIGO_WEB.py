b50c4c85d68f0c447f279c27d65993984a708937
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2023-05-21 13:37:43 -0500

    Add files via upload

--- CÓDIGO_WEB.py
+++ CÓDIGO_WEB.py
@@ -102,17 +102,5 @@ df_seleccion = df2.query("Indicador == @Activo" ) #el primero es la columna y el
 
 fig1 = px.line( x='fecha', y='valor', title='Gráfica USD/COP', data_frame = df_seleccion, color = 'Indicador')
 
-fig1.update_xaxes(
-        rangeslider_visible=True,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-)
 
 st.write(fig1)
